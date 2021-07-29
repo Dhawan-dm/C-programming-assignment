c95724dedda93759ced72e6e8f6313351a7d7aae
vagrant <vagrant@myvm18>
2021-07-29 23:46:26 +0000

    ques 11 of assignment

---
+++ ques11.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+#include <string.h>
+
+
+int main()
+{
+        char str[100];
+        char str2[100];
+	scanf("%[^\n]%*c", str);
+	scanf("%[^\n]%*c", str2);
+
+	
+
+	int n = strlen(str);
+	int n2 = strlen(str2);
+	int count=0;
+
+	for(int i =0 ;i <n; i++){
+	    
+	    for(int j =0 ;j<n2; j++){
+			
+			count=1;
+			if(str[i]==str2[j])
+			{
+			    count=0;
+			    printf("%d",i);
+			    break;
+			    
+			}
+		
+    	}	
+		if(count==0)
+		{
+		    break;
+		}
+	}
+	if(count==1)
+	{
+	    printf("-1");
+	}
+
+
+
+
+   	
+
+   	return 0;
+}
+
